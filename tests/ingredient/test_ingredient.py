8fff481c4110442c646a11c3b45b5cf6724a5515
thais viana <thaisviana081@outlook.com>
2023-04-21 13:31:36 -0300

    1 - Testando classes já implementadas parte 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,15 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
 
 
 # Req 1
 def test_ingredient():
-    pass
+    instance_test_1 = Ingredient('manteiga')
+    instance_test_2 = Ingredient('alho')
+
+    assert instance_test_1 == instance_test_1
+    assert instance_test_1 != instance_test_2
+    assert hash(instance_test_1) == hash(instance_test_1)
+    assert hash(instance_test_1) != hash(instance_test_2)
+    assert repr(instance_test_2) == "Ingredient('alho')"
+    assert instance_test_2.name == "alho"
+    assert instance_test_2.restrictions == set()
